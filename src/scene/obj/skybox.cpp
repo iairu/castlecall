2c141d41938ac24ac162e812c3f6f945cbe6edb7
Marek Klanica <marek8299@outlook.com>
2021-12-15 21:15:15 +0100

    Make skybox texture always visible

--- src/scene/obj/skybox.cpp
+++ src/scene/obj/skybox.cpp
@@ -1,15 +1,15 @@
 #include "skybox.h"
 #include "paths.h"
 
-#include <shaders/diffuse_vert_glsl.h>
-#include <shaders/diffuse_frag_glsl.h>
+#include <shaders/texture_vert_glsl.h>
+#include <shaders/texture_frag_glsl.h>
 
 std::unique_ptr<ppgso::Mesh> Skybox::mesh;
 std::unique_ptr<ppgso::Texture> Skybox::texture;
 std::unique_ptr<ppgso::Shader> Skybox::shader;
 
 Skybox::Skybox() {
-    if (!shader) shader = std::make_unique<ppgso::Shader>(diffuse_vert_glsl, diffuse_frag_glsl);
+    if (!shader) shader = std::make_unique<ppgso::Shader>(texture_vert_glsl, texture_frag_glsl);
     if (!texture) texture = std::make_unique<ppgso::Texture>(ppgso::image::loadBMP(TEXTURE_PATH "skybox.bmp"));
     if (!mesh) mesh = std::make_unique<ppgso::Mesh>(OBJ_PATH "skybox.obj");
 }
@@ -17,9 +17,6 @@ Skybox::Skybox() {
 void Skybox::render(Scene &scene) {
 
     shader->use();
-
-    // Set up light
-    shader->setUniform("LightDirection", scene.lightDirection);
 
     // use camera
     shader->setUniform("ProjectionMatrix", scene.camera->projectionMatrix);
--- src/scene/obj/water.cpp
+++ src/scene/obj/water.cpp
@@ -1,8 +1,6 @@
 #include "water.h"
 #include "paths.h"
 
-#include <shaders/diffuse_vert_glsl.h>
-#include <shaders/diffuse_frag_glsl.h>
 #include <shaders/texture_frag_glsl.h>
 #include <shaders/texture_vert_glsl.h>
 
